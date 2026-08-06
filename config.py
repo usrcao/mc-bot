07f72616d432a2b5e98b250c27a1984234087527
usrcao <130359691+usrcao@users.noreply.github.com>
2026-08-06 14:28:13 +0800

    Add server list and allowed group ID to config
    Add server configuration and allowed group ID.

--- config.py
+++ config.py
@@ -1,1 +1,7 @@
+SERVERS = [
+    {"name": "0服", "host": "gameserver-ningbo-bgp-01.cn.shiyu.io", "port": 32060},
+    {"name": "1服", "host": "gameserver-ningbo-bgp-01.cn.shiyu.io", "port": 32061},
+    {"name": "2服", "host": "gameserver-ningbo-bgp-01.cn.shiyu.io", "port": 32062},
+]
 
+ALLOWED_GROUP_ID = 814196895  # 改成你的QQ群号
